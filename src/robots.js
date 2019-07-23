9c361d6e34b26c9411fb9f16f3a6304e01f6a94f
MoniqueCastro <moniquelimacastro@gmail.com>
2019-07-23 20:25:31 +0200

    first commit

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Jane Doe',
+	username: 'doey',
+	email: 'jane.doe@gmail.com'
+	},
+{
+	id: 2,
+	name: 'John Doe',
+	username: 'joe',
+	email: 'john.doe@gmail.com'
+},
+{
+	id: 3,
+	name: 'Jim Doe',
+	username: 'jimmy',
+	email: 'jim.doe@gmail.com'
+},
+{
+	id: 4,
+	name: 'Jonathan Doe',
+	username: 'doehead',
+	email: 'jonathan.doe@gmail.com'
+},
+{
+	id:5 ,
+	name: 'Jennifer Doe',
+	username: 'jenny',
+	email: 'jenny.doe@gmail.com'
+}
+];
