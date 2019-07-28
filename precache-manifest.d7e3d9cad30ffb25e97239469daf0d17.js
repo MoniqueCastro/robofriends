7c6ee093e1d23e68d6c8800823b123e62b62e661
MoniqueCastro <moniquelimacastro@gmail.com>
2019-07-28 11:08:49 +0200

    Updates

---
+++ precache-manifest.d7e3d9cad30ffb25e97239469daf0d17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "c035f2ec5aa2e92fc894",
+    "url": "/robofriends/static/js/main.c035f2ec.chunk.js"
+  },
+  {
+    "revision": "de7c44520267c9d2be9c",
+    "url": "/robofriends/static/js/1.de7c4452.chunk.js"
+  },
+  {
+    "revision": "c035f2ec5aa2e92fc894",
+    "url": "/robofriends/static/css/main.d8712a14.chunk.css"
+  },
+  {
+    "revision": "de7c44520267c9d2be9c",
+    "url": "/robofriends/static/css/1.7a153874.chunk.css"
+  },
+  {
+    "revision": "ee53b500a1ed368f1743993994e286c0",
+    "url": "/robofriends/index.html"
+  }
+];
